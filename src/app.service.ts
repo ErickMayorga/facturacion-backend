c425d2588a60aae7970a957e32cbae3eb82d4073
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-06-30 15:45:07 -0500

    update: Changing message in Nest

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Esta es una prueba de producción con Heroku';
+    return 'Esta es una prueba de producción continua con Heroku!';
   }
 }
