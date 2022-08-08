77b18abd27aa64987192d38a72b5f6bded763ed1
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-08-07 23:08:42 -0500

    Merge remote-tracking branch 'github/master'

--- src/modelo/producto/dto/producto.entity.ts
+++ src/modelo/producto/dto/producto.entity.ts
@@ -1,6 +1,6 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
-@Entity('transportista')
+@Entity('producto')
 export class ProductoEntity {
     @PrimaryGeneratedColumn()
     id_producto: number
